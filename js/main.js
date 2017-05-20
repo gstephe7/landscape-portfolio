c6e0672a8ecb97e030b033b19ec94c383b6d7925
Greg <gstephe7@gmail.com>
2017-05-19 23:05:47 -0400

    finished about section and projects section of modal

--- js/main.js
+++ js/main.js
@@ -31,27 +31,68 @@ var page = new Vue({
 	    background: 'Hello, my name is Greg Stephens and I\'m a Front-End Web Developer from Athens, GA. I pride myself on being creative, easy-going, and driven to grow within my career.',
 	    skills: {
 		    title: 'SKILLS',
-		    skills: [
+		    skillsLt: [
 		      'JavaScript',
 		      'Vue.js',
 		      'jQuery',
 		      'HTML5',
 		      'CSS3',
 		      'Bootstrap',
-		      'Git',
-		      'SEO',
+		      'Git'
+		    ],
+        skillsRt: [
+          'SEO',
 		      'Google Analytics',
 		      'Google Adsense',
 		      'Google Adwords',
 		      'Teaching',
 		      'Blogging',
 		      'Studio One'
-		    ]
-	    }
+        ]
+	    },
+      education: {
+        title: 'EDUCATION',
+        udacity: {
+          name: 'Udacity',
+          degree: 'Nanodegree',
+          major: 'Front-End Web Development',
+          dates: '2017',
+          img: 'img/udacity.jpg'
+        },
+        kennesaw: {
+          name: 'Kennesaw State University',
+          degree: 'Bachelor\'s',
+          major: 'History Education',
+          dates: '2009-2012',
+          img: 'img/ksu.jpg'
+        }
+      },
+      interests: {
+        title: 'INTERESTS',
+        interestsLt: [
+          'Being a Dad',
+          'Music',
+          'Creating things',
+          'Politics',
+          'Economics'
+        ],
+        interestsRt: [
+          'History',
+          'Investing',
+          'Georgia Bulldogs Football',
+          'Atlanta United Soccer',
+          'Louis CK\'s Comedy'
+        ]
+      }
 	  },
     projectsSection: {
       displayProjects: false,
-      title: 'PROJECTS'
+      title: 'PROJECTS',
+      arcadeGame: {
+        name: 'Frogger: Presidential Edition',
+        img: 'img/arcadegame.jpg',
+        description: 'Created a clone of the classic arcade game, Frogger. This Udacity project was completed using object-oriented JavaScript and HTML5 Canvas. I put my own spin on this game by using presidential candidates as the players.'
+      }
     },
     contactSection: {
       displayContact: false,
@@ -81,148 +122,4 @@ var page = new Vue({
       this.contactSection.displayContact = !this.contactSection.displayContact;
     }
   }
-})
-
-
-/*class Post {
-  constructor(title, link, author, img) {
-    this.title = title;
-    this.link = link;
-    this.author = author;
-    this.img = img;
-  }
-}
-
-const app = new Vue({
-  el: '#nav',
-  data: {
-    keyword: "",
-    postList: [
-      new Post(
-        'Vue.js',
-        'https://vuejs.org/',
-        'Chris',
-        'https://vuejs.org//images/logo.png'
-      ),
-      new Post(
-        'React.js',
-        'https://facebook.github.io/react/',
-        'Tim',
-        'http://daynin.github.io/clojurescript-presentation/img/react-logo.png'
-      ),
-      new Post(
-        'Angular.js',
-        'https://angularjs.org/',
-        'Sam',
-        'https://angularjs.org/img/ng-logo.png'
-      ),
-      new Post(
-        'Ember.js',
-        'http://emberjs.com/',
-        'Rachel',
-        'http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200'
-      ),
-      new Post(
-        'Meteor.js',
-        'https://www.meteor.com/',
-        'Chris',
-        'http://hacktivist.in/introduction-to-nodejs-mongodb-meteor/img/meteor.png'
-      ),
-      new Post(
-        'Aurelia',
-        'http://aurelia.io/',
-        'Tim',
-        'https://cdn.auth0.com/blog/aurelia-logo.png'
-      ),
-      new Post(
-        'Node.js',
-        'https://nodejs.org/en/',
-        'A. A. Ron',
-        'https://code-maven.com/img/node.png'
-      ),
-      new Post(
-        'Pusher',
-        'https://pusher.com/',
-        'Alex',
-        'https://avatars1.githubusercontent.com/u/739550?v=3&s=400'
-      ),
-      new Post(
-        'Feathers.js',
-        'http://feathersjs.com/',
-        'Chuck',
-        'https://cdn.worldvectorlogo.com/logos/feathersjs.svg'
-      ),
-    ]
-  },
-  methods: {
-    toggleOnOff: function() {
-      this.onOff = !this.onOff;
-    }
-  },
-  computed: {
-    filteredList: function() {
-      return this.postList.filter((post) => {
-        return post.title.toLowerCase().includes(this.keyword.toLowerCase());
-      });
-    }
-  }
-});*/
-
-
-/*var about = {
-  summary: {
-    title: '<div class="col-xs-12"><h3 class="title">ABOUT</h3></div>',
-    img: '<div class="col-xs-3"><img src="img/profilepic.jpg" class="profile-pic img-circle"></div>',
-    intro: '<div class="col-xs-9"><p class="info-text">Hello, my name is Greg Stephens. I\'m a Front-End Web Developer transitioning from a 3 year career as a teacher. I\'m a flexible and creative professional with a strong desire to learn and grow within a new industry.</p></div>'
-  },
-  skills: {
-    title: '<div class="col-xs-12"><h3 class="title">SKILLS</h3></div>',
-    skills1: '<div class="col-xs-6"><ul class="skills text-center"><li class="info-text">JavaScript</li><li class="info-text">HTML</li><li class="info-text">CSS</li><li class="info-text">jQuery</li></ul></div>',
-    skills2: '<div class="col-xs-6"><ul class="skills text-center"><li class="info-text">Bootstrap</li><li class="info-text">Git</li><li class="info-text">Teaching</li><li class="info-text">SEO</li></ul></div>'
-  }
-};
-
-function aboutSection() {
-  $('.info').empty();
-  $('.info').append('<div class="row">' + about.summary.title + '</div><div class="row">' + about.summary.img + about.summary.intro + '</div><hr><div class="row>' + about.skills.title + '</div><div class="row">' + about.skills.skills1 + about.skills.skills2 + '</div><hr>');
-  $('.info').slideToggle(1000);
-};
-
-//moves spaceship down
-function moveSpaceship() {
-  $('.spaceship').animate({
-    'marginTop' : "+=200px"
-  }, 1000);
-}
-
-//moves spaceship up
-function hideSpaceship() {
-  $('.spaceship').animate({
-    'marginTop' : "-=200px"
-  }, 1000);
-}
-
-$('.about-btn').click(function() {
-    moveSpaceship();
-    setTimeout(function() {
-      hideSpaceship();
-    }, 2000);
-    aboutSection();
 });
-
-$('.projects-btn').click(function() {
-  moveSpaceship();
-  setTimeout(function() {
-    hideSpaceship();
-  }, 2000);
-
-});
-
-$('.contact-btn').click(function() {
-  moveSpaceship();
-  setTimeout(function() {
-    hideSpaceship();
-  }, 2000);
-
-});
-*/
